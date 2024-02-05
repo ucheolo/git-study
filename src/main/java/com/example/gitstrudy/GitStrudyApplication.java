18e7c2c79c82a24e017957f97ed9d42b3ee51fe0
ucShin <swc9704@gmail.com>
2024-02-05 15:35:34 +0900

    init commint

---
+++ src/main/java/com/example/gitstrudy/GitStrudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitstrudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStrudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitStrudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitstrudy/GitStrudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitstrudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStrudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
