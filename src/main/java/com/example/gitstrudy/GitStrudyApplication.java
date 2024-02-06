9ff8369c5d6ded097d5d4b0b95436923b292c4bc
ucShin <swc9704@gmail.com>
2024-02-06 17:24:01 +0900

    ãa, b, c  print

--- src/main/java/com/example/gitstrudy/GitStrudyApplication.java
+++ src/main/java/com/example/gitstrudy/GitStrudyApplication.java
@@ -3,11 +3,17 @@ package com.example.gitstrudy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.Clock;
+
 @SpringBootApplication
 public class GitStrudyApplication {
 
     public static void main(String[] args) {
         System.out.println("new line");
+        System.out.println("");
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
         SpringApplication.run(GitStrudyApplication.class, args);
     }
 
